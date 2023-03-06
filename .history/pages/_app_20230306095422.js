62d2d02602cd68de979fbcc16148041d288fb566
JacobSkilling-dot <113203264+JacobSkilling-dot@users.noreply.github.com>
2023-03-06 10:38:36 -0800

    commit 1

---
+++ .history/pages/_app_20230306095422.js
@@ -0,0 +1,24 @@
+import '@/styles/globals.css'
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+
+import en from '../i18n/en.json';
+import fr from '../i18n/fr.json';
+
+const message = {
+  en,
+  fr
+}
+
+function getDirection(locale) {
+  return "litr";
+}
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+  <Component {...pageProps} />
+  </IntlProvider>
+  )
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Language Translation
+
+## languages
+- english
+- french
+
+## Installation
+```
+npm install next-i18next
+```
+```
+npm i react-intl
+```
